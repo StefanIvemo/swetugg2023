bcb7841028c13bb6e4df8fb12ec967c2ab14c152
Stefan Ivemo <stefan@ivemo.se>
2023-02-06 12:37:06 +0100

    Changed spec to Key Vault

--- supportFiles/templateSpecs.ps1
+++ supportFiles/templateSpecs.ps1
@@ -1,17 +1,17 @@
 # Create a template spec named storageSpec in resource group swetugg-2023-rg
 $parameters=@{
-    Name = 'storageSpec'
+    Name = 'keyVaultSpec'
     Version = '1.0'
     ResourceGroupName = 'swetugg-2023-rg'
     Location = 'westeurope'
-    TemplateFile = './modules/storage_generic.bicep'
+    TemplateFile = './modules/keyvault_generic.bicep'
     Tag=@{Dept="Integrations";Environment="Production"}
 }
 
 New-AzTemplateSpec @parameters
 
-# Deploy storage account using template spec
-$id = (Get-AzTemplateSpec -Name 'storageSpec' -ResourceGroupName 'swetugg-2023-rg' -Version '1.0').Versions.Id
+# Deploy Key Vault using template spec
+$id = (Get-AzTemplateSpec -Name 'keyVaultSpec' -ResourceGroupName 'swetugg-2023-rg' -Version '1.0').Versions.Id
 
 $mySpec = @{
     TemplateSpecId = $id
